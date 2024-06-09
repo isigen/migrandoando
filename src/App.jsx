ba66cc338b293096dea1e9289ba8d814e0173c3c
isigen <isidora.abarzua@genstudents.org>
2024-06-09 18:45:11 -0400

    ssss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-// src/App.jsx
 import React from "react";
 import "./App.css";
-import Registroc from "./components/Registroc";
+import Header from "./components/Header";
+import Form from "./components/Form";
 
 function App() {
   return (
-    <div>
-      <Registroc />
+    <div className="container-fluid">
+      <Header/>
+      <Form/>
     </div>
   );
 }
